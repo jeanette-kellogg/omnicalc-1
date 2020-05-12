337aa5675c83865967672daa4a7ce780ea4b8daa
jeanettestock <jeanettemstock@gmail.com>
2020-05-12 04:17:32 +0000

    Square root added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,18 @@ class ApplicationController < ActionController::Base
     render ({ :template => "calculation_templates/square_form.html.erb"})
     end
     def blank_square_results
+      @num = params.fetch("number").to_f
+      @square_num = @num*@num.to_f
     render ({ :template => "calculation_templates/square_results.html.erb"})
     end
 
     def blank_square_root_form
     render ({ :template => "calculation_templates/square_root_form.html.erb"})
+    end
+    def blank_square_root_results
+      @num = params.fetch("number").to_f
+      @square_root_num = @num.to_f ** 0.5
+    render ({ :template => "calculation_templates/square_root_results.html.erb"})
     end
 
     def blank_payment_form
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   get("/square/results",{ :controller => "application", :action => "blank_square_results"}) 
 
   get("/square_root/new",{ :controller => "application", :action => "blank_square_root_form"}) 
-
+  get("/square_root/results",{ :controller => "application", :action => "blank_square_root_results"}) 
 
   get("/payment/new",{ :controller => "application", :action => "blank_payment_form"}) 
-
+  get("/payment/results",{ :controller => "application", :action => "blank_payment_results"}) 
 
   get("/random/new",{ :controller => "application", :action => "blank_random_form"}) 
+    get("/random/results",{ :controller => "application", :action => "blank_random_results"}) 
 
   # Write your routes above this line
   # =================================
